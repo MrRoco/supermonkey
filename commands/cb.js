00aefacf5b7a4d3f3f04b2629923e75e242500fd
Roel Zeilstra <roelzeilstra@outlook.com>
2020-11-06 23:33:10 +0100

    Correct commands

--- commands/cb.js
+++ commands/cb.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Clan Battles!',
 	execute(message) {
         message.channel.send('@globbites we have clan battles tonight at 19.00 CET / 18.00 GMT. React on berk if you are joining');
-        const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'berk');
+        const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'Berk');
 	    message.react(reactionEmoji);
 	},
 };
