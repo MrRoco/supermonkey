cd69307a2692f8be63a4f9747c889d8a2c4efef7
Roel Zeilstra <roelzeilstra@outlook.com>
2020-11-06 23:46:04 +0100

    Add react to correct message

--- commands/cb.js
+++ commands/cb.js
@@ -2,8 +2,7 @@ module.exports = {
 	name: 'cb',
 	description: 'Clan Battles!',
 	execute(message) {
-        message.channel.send('@globbites we have clan battles tonight at 19.00 CET / 18.00 GMT. React on berk if you are joining');
         const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'Berk');
-	    message.react(reactionEmoji);
+        message.channel.send('@globbites we have clan battles tonight at 19.00 CET / 18.00 GMT. React on berk if you are joining').react(reactionEmoji);
 	},
 };
