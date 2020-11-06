b1ffe36d4a6de27f023e292228e116d67425d822
Roel Zeilstra <roelzeilstra@outlook.com>
2020-11-07 00:26:21 +0100

    Update cb.js

--- commands/cb.js
+++ commands/cb.js
@@ -4,6 +4,6 @@ module.exports = {
 	execute(message) {
         const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'Berk');
         message.channel.send('@globbites we have clan battles tonight at 19.00 CET / 18.00 GMT. React on berk if you are joining');
-        message.channel.send(str(reactionEmoji));
+        message.channel.send(toString(reactionEmoji));
 	},
 };
